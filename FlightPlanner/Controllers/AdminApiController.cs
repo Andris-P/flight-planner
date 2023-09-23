b23fe6db9e0a4c41933bc2f218875983114f6653
Andris Pokals <andris.pokals@gmail.com>
2023-09-23 14:09:13 +0300

    first commit

---
+++ FlightPlanner/Controllers/AdminApiController.cs
@@ -0,0 +1,37 @@
+﻿using FlightPlanner.Models;
+using FlightPlanner.Storage;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner.Controllers
+{
+    [Authorize]
+    [Route("admin-api")]
+    [ApiController]
+    public class AdminApiController : ControllerBase
+    {
+        private readonly FlightStorage _storage;
+        
+        public AdminApiController()
+        {
+            _storage = new FlightStorage();
+        }
+
+        [Route("flights/{id}")]
+        [HttpGet]
+       public IActionResult GetFlight(int id)
+        {
+            return NotFound();
+        }
+
+        [Route("flights")]
+        [HttpPut]
+        public IActionResult PutFlight(Flight flight)
+        {
+            _storage.AddFlight(flight);
+
+            return Created("",flight);
+        }
+    }
+}
+ 
